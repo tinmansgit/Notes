0f9b079bdc2f56e29aa2d64bc142b7b0805b330d
Tin Man's Git <102759745+tinmansgit@users.noreply.github.com>
2025-08-04 17:36:14 +0000

    Update my_note.sh

--- my_note.sh
+++ my_note.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# change to a location on your system
 cd /media/coder/AV/Notes
 
 date_str=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -8,4 +9,5 @@ filename="${date_str}.md"
 
 touch "$filename"
 
+# change to your choice terminal 
 terminator --geometry=600x600+700+200 -x vi "$filename"
